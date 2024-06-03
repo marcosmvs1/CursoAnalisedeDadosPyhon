3acb2a821981ec7d077c6ed8e5c4be24e69c366c
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-06-03 11:12:52 -0300

    Create Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+pd.Series(data=5) 
+#Cria uma série com o valor a
+
+lista_nomes = 'Marcos Tathy Stella Mãe Natanael'.split()
+ 
+pd.Series(lista_nomes)
+#Cria uma Séries com uma lista de nomes 
+
