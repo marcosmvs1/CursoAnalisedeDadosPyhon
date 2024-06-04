ff07f02ee424a99ea70718ed14b6f113b1fdfa7f
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-06-04 11:46:01 -0300

    Aula1
    Parei na ( SELEÇÃO DE COLUNAS EM UM DATAFRAME)

--- Aula1.py
+++ Aula1.py
@@ -1,10 +1,21 @@
 import pandas as pd
 
-pd.Series(data=5) 
-#Cria uma série com o valor a
+lista_nomes = 'Howard Ian Peter Jonah Kellie'.split()
+lista_cpfs = '111.111.111-11 222.222.222-22 333.333.333-33 444.444.444-44 555.555.555-55'.split()
+lista_emails = 'risus.varius@dictumPhasellusin.ca Nunc@vulputate.ca fames.ac.turpis@cursusa.org non@felisullamcorper.org eget.dictum.placerat@necluctus.co.uk'.split()
+lista_idades = [32, 22, 25, 29, 38]
+pd.DataFrame(lista_nomes, columns=['nome'])
 
-lista_nomes = 'Marcos Tathy Stella Mãe Natanael'.split()
- 
-pd.Series(lista_nomes)
-#Cria uma Séries com uma lista de nomes 
+# Criando o DataFrame
+df = pd.DataFrame({
+    'Nome': lista_nomes,
+    'CPF': lista_cpfs,
+    'Email': lista_emails,
+    'Idade': lista_idades
+})
+
+# Exibindo o DataFrame
+print(df)
+
+#SELEÇÃO DE COLUNAS EM UM DATAFRAME(parei aqui)
 
